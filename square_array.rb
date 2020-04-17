2e7531803dd031e007a57e9a83bac532fe1afa88
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-17 03:49:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 square_array = [5, 8, 13, 21]
 
-def square_array(array)
-  square_array.each do |array|
+def square_array(numbers)
+  square_array.each do |numbers|
     puts array*array
   end
 end
