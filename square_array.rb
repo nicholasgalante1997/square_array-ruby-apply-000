36a0a509a8a386039ce3effe278c7257f2404ec0
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-17 04:05:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ square_array = [1, 2, 3]
 count = 1
 def square_array(numbers)
   square_array.each do |numbers|
-    puts (#{numbers}*#{numbers})
+    puts "(#{numbers}*#{numbers})"
     count += 1 
   end
 end
